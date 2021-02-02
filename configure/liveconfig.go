9c6f83d1a0d56f753208bf153db1fca094761d9a
happylay <1803455421@qq.com>
2021-02-02 21:20:00 +0800

    💖 修改配置

--- configure/liveconfig.go
+++ configure/liveconfig.go
@@ -61,9 +61,9 @@ type ServerCfg struct {
 
 // default config
 var defaultConf = ServerCfg{
-	ConfigFile:      "livego.yaml",
-	FLVArchive:	false,
-	RTMPNoAuth:	false,
+	ConfigFile:      "config/config.yaml",
+	FLVArchive:      false,
+	RTMPNoAuth:      false,
 	RTMPAddr:        ":1935",
 	HTTPFLVAddr:     ":7001",
 	HLSAddr:         ":7002",
@@ -82,8 +82,10 @@ var defaultConf = ServerCfg{
 	}},
 }
 
+// 初始化配置文件对象
 var Config = viper.New()
 
+// 初始化日志
 func initLog() {
 	if l, err := log.ParseLevel(Config.GetString("level")); err == nil {
 		log.SetLevel(l)
@@ -94,70 +96,71 @@ func initLog() {
 func init() {
 	defer Init()
 
-	// Default config
+	// 默认配置
 	b, _ := json.Marshal(defaultConf)
 	defaultConfig := bytes.NewReader(b)
 	viper.SetConfigType("json")
-	viper.ReadConfig(defaultConfig)
-	Config.MergeConfigMap(viper.AllSettings())
+	_ = viper.ReadConfig(defaultConfig)
+	_ = Config.MergeConfigMap(viper.AllSettings())
 
-	// Flags
-	pflag.String("rtmp_addr", ":1935", "RTMP server listen address")
-	pflag.String("httpflv_addr", ":7001", "HTTP-FLV server listen address")
-	pflag.String("hls_addr", ":7002", "HLS server listen address")
-	pflag.String("api_addr", ":8090", "HTTP manage interface server listen address")
-	pflag.String("config_file", "livego.yaml", "configure filename")
-	pflag.String("level", "info", "Log level")
-	pflag.Bool("hls_keep_after_end", false, "Maintains the HLS after the stream ends")
-	pflag.String("flv_dir", "tmp", "output flv file at flvDir/APP/KEY_TIME.flv")
-	pflag.Int("read_timeout", 10, "read time out")
-	pflag.Int("write_timeout", 10, "write time out")
-	pflag.Int("gop_num", 1, "gop num")
+	// 命令行标志
+	pflag.String("rtmp_addr", ":1935", "RTMP服务监听地址")
+	pflag.String("httpflv_addr", ":7001", "HTTP-FLV服务监听地址")
+	pflag.String("hls_addr", ":7002", "HLS服务监听地址")
+	pflag.String("api_addr", ":8090", "HTTP接口管理服务监听地址")
+	pflag.String("config_file", "config/config.yaml", "配置文件名称")
+	pflag.String("level", "info", "日志等级")
+	pflag.Bool("hls_keep_after_end", false, "流结束后保持HLS")
+	pflag.String("flv_dir", "tmp", "输出flv文件路径：flvDir/APP/KEY_TIME.flv")
+	pflag.Int("read_timeout", 10, "读超时时间")
+	pflag.Int("write_timeout", 10, "写超时时间")
+	pflag.Int("gop_num", 1, "gop数量")
 	pflag.Parse()
-	Config.BindPFlags(pflag.CommandLine)
+	_ = Config.BindPFlags(pflag.CommandLine)
 
-	// File
+	// 设置配置文件
 	Config.SetConfigFile(Config.GetString("config_file"))
+	// 配置文件路径
 	Config.AddConfigPath(".")
 	err := Config.ReadInConfig()
 	if err != nil {
 		log.Warning(err)
-		log.Info("Using default config")
+		log.Info("使用默认配置")
 	} else {
-		Config.MergeInConfig()
+		_ = Config.MergeInConfig()
 	}
 
-	// Environment
+	// 环境
 	replacer := strings.NewReplacer(".", "_")
 	Config.SetEnvKeyReplacer(replacer)
 	Config.AllowEmptyEnv(true)
 	Config.AutomaticEnv()
 
-	// Log
+	// 日志
 	initLog()
 
-	// Print final config
+	// 打印最终配置
 	c := ServerCfg{}
-	Config.Unmarshal(&c)
-	log.Debugf("Current configurations: \n%# v", pretty.Formatter(c))
+	_ = Config.Unmarshal(&c)
+	log.Debugf("当前配置：\n%# v", pretty.Formatter(c))
 }
 
-func CheckAppName(appname string) bool {
+func CheckAppName(appName string) bool {
 	apps := Applications{}
-	Config.UnmarshalKey("server", &apps)
+	_ = Config.UnmarshalKey("server", &apps)
 	for _, app := range apps {
-		if app.Appname == appname {
+		if app.Appname == appName {
 			return app.Live
 		}
 	}
 	return false
 }
 
-func GetStaticPushUrlList(appname string) ([]string, bool) {
+func GetStaticPushUrlList(appName string) ([]string, bool) {
 	apps := Applications{}
-	Config.UnmarshalKey("server", &apps)
+	_ = Config.UnmarshalKey("server", &apps)
 	for _, app := range apps {
-		if (app.Appname == appname) && app.Live {
+		if (app.Appname == appName) && app.Live {
 			if len(app.StaticPush) > 0 {
 				return app.StaticPush, true
 			} else {

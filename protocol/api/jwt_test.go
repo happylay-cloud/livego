4f3d9460e4411890f9cac6f5433ee090d5e10899
happylay <1803455421@qq.com>
2021-02-08 21:03:46 +0800

    🎉 添加jwt测试类过期时间（默认一天）

--- protocol/api/jwt_test.go
+++ protocol/api/jwt_test.go
@@ -3,6 +3,7 @@ package api
 import (
 	"fmt"
 	"testing"
+	"time"
 
 	"github.com/dgrijalva/jwt-go"
 )
@@ -16,6 +17,7 @@ func TestJWTMiddleware(t *testing.T) {
 	// 设置载荷
 	tokenObj.Claims = jwt.MapClaims{
 		"live": "true",
+		"exp":  time.Now().Add(24 * time.Hour).Unix(),
 	}
 	// 签名
 	token, _ := tokenObj.SignedString([]byte("test"))
@@ -26,5 +28,5 @@ func TestJWTMiddleware(t *testing.T) {
 	token = fmt.Sprintf("bearer %v", token)
 	fmt.Println("Authorization", token)
 
-	// Authorization bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJsaXZlIjoidHJ1ZSJ9.h-RiquHM5wt13ut17MPxCasJFCFolC-irj4KbPXeSQM
+	// Authorization bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2MTI4NzU3NTAsImxpdmUiOiJ0cnVlIn0.LIX2aWcw4kBuK3fALtQO93dp0kZe7C7RkeK0SqegXQI
 }
